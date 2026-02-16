f4b37afc0826f30a9ba753c0d8f041777aa2f51d
YoussefMohamed <ym0998682@gmail.com>
2026-02-16 18:10:08 +0200

    update project configuration and improve ingredient rendering logic

--- apps/SuperFitnessApp/src/app/app.routes.server.ts
+++ apps/SuperFitnessApp/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'class/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'single-meal/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
